a48edfcd0cd3adaaaf751e4f895471a461a6b821
Enze Zhu <enzezhuucas@gmail.com>
2024-08-22 13:11:39 +0800

    Update vaihingen_dataset.py

--- unetmamba_model/datasets/vaihingen_dataset.py
+++ unetmamba_model/datasets/vaihingen_dataset.py
@@ -55,7 +55,7 @@ def val_aug(img, mask):
 
 
 class VaihingenDataset(Dataset):
-    def __init__(self, data_root='autodl-tmp/data/vaihingen/test_'+str(IMG_SIZE), mode='val', img_dir='images', mask_dir='masks',
+    def __init__(self, data_root='data/vaihingen/test_'+str(IMG_SIZE), mode='val', img_dir='images', mask_dir='masks',
                  img_suffix='.tif', mask_suffix='.png', transform=val_aug, mosaic_ratio=0.0,
                  img_size=ORIGIN_IMG_SIZE):
         self.data_root = data_root
